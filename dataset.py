6ad59006e538cfe00f33c26a42118cf41fbbe26c
Lucas <lmlwojcik@inf.ufpr.br>
2026-05-19 15:07:54 -0300

    [add] mixed datasets

--- dataset.py
+++ dataset.py
@@ -4,7 +4,6 @@ import cv2
 
 import random
 import json
-from time import time
 
 def resize_with_pad(image, 
                     new_shape, 
@@ -102,31 +101,31 @@ class DynamicDataset(Dataset):
             return im1, im2, lb
 
 class StaticDataset(Dataset):
-    def __init__(self, protocol_file, size=[256, 256], partition='train',
-                device='cpu', seed=None):
+    def __init__(self, protocol_file, size=[224, 224], partition='train',
+                device='cuda:0', seed=None):
         with open(protocol_file, "r") as fd:
             self.protocol = json.load(fd)
+            self.data = self.protocol['data']
         
         if partition == 'train':
-            self.dataset = self.protocol['training_instances']
+            self.dataset = self.data['train_instances']
             self.use_triplet = self.protocol['config']['type'] == "triplet"
         else:
             self.use_triplet = False
-            elif partition == 'val':
-                self.dataset = self.protocol['val_instances']
+            if partition == 'val':
+                self.dataset = self.data['val_instances']
             elif partition == 'test_known':
-                self.dataset = self.protocol['test_instances_known']
+                self.dataset = self.data['test_instances_known']
             elif partition == 'test_mixed':
-                self.dataset = self.protocol['test_instances_mixed']
+                self.dataset = self.data['test_instances_mixed']
             elif partition == 'test_unique':
-                self.dataset = self.protocol['test_instances_unique']
+                self.dataset = self.data['test_instances_unique']
 
         self.device = device
         self.size = size
 
-        if seed is None:
-            seed = int(time.time())
-        self.seed = seed
+        if seed is not None:
+            self.seed = seed
         # Seed for random is set on main, this is just for logging
 
     def __len__(self):
@@ -149,3 +148,67 @@ class StaticDataset(Dataset):
             im2 = load_image(im2_file, self.size, self.device)
 
             return im1, im2, lb
+
+
+class MixedDatasetTrain(Dataset):
+    def __init__(self, protocol_file, size=[224, 224], partition='train',
+                device='cuda:0', seed=None, steps=1000):
+
+        with open(protocol_file, "r") as fd:
+            protocol = json.load(fd)
+
+        self.protocol = protocol['train']
+        self.cams = list(self.protocol.keys())
+
+        self.device = device
+        self.size = size
+        self.steps = steps
+
+        if seed is not None:
+            self.seed = seed
+
+    def __len__(self):
+        return self.steps
+
+    def __getitem__(self, idx):
+        anchor_class, neg_class = random.sample(self.cams, k=2)
+
+        anchor_file, pos_file = random.sample(self.protocol[anchor_class], k=2)
+        neg_file = random.choice(self.protocol[neg_class])
+
+        anchor = load_image(f"datasets/images/" + anchor_file + ".jpg", self.size, self.device)
+        pos = load_image(f"datasets/images/" + pos_file + ".jpg", self.size, self.device)
+        neg = load_image(f"datasets/images/" + neg_file + ".jpg", self.size, self.device)
+
+        return anchor, pos, neg
+
+class MixedDatasetEval(Dataset):
+    def __init__(self, protocol_file, size=[224, 224], partition='valid',
+                device='cuda:0', seed=None):
+
+        with open(protocol_file, "r") as fd:
+            protocol = json.load(fd)
+
+        self.protocol = protocol[partition]
+        self.dataset = self.protocol['pairs']
+        random.shuffle(self.dataset)
+
+        self.device = device
+        self.size = size
+
+        if seed is not None:
+            self.seed = seed
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def __getitem__(self, idx):
+        im1_file, im2_file, label = self.dataset[idx]
+        label = 1 if label else 0
+
+        lb = torch.Tensor([label]).to(self.device).to(torch.long)
+        im1 = load_image(f"datasets/images/" + im1_file + ".jpg", self.size, self.device)
+        im2 = load_image(f"datasets/images/" + im2_file + ".jpg", self.size, self.device)
+
+        return im1, im2, lb
+
